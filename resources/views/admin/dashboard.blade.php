5de0b4bd1e767b8e57459cb38147ccd2d30a4578
AntonioDattolo <antonio19794.a@gmail.com>
2024-07-09 17:37:38 +0200

    Funciont-method fixed route with route('ecc')

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
     <div class="container-fluid mt-4">
-        <a href="{{ route(admin . project . index) }}">Prova index</a>
+        <a href="{{route('admin.Project.index')}}">Prova index</a>
         <div class="row justify-content-center">
             <div class="col-md-8">
                 <div class="card">
