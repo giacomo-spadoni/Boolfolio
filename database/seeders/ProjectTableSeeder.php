94aea038d7427239a806e1f804d1caba22294bbc
Luca-Mariselli <lucamariselli@gmail.com>
2024-07-10 15:45:57 +0200

    add create form

--- database/seeders/ProjectTableSeeder.php
+++ database/seeders/ProjectTableSeeder.php
@@ -2,16 +2,24 @@
 
 namespace Database\Seeders;
 
+use App\Models\Project;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
 
 class ProjectTableSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
-    public function run(): void
+    public function run(Faker $faker): void
     {
-        //
+        for ($i=0; $i < 10; $i++) { 
+            $newProject = new Project();
+            $newProject->title = $faker->sentence(2);
+            $newProject->description = $faker->sentence(22);
+            $newProject->img = "https://picsum.photos/id/" . rand(1, 500) . "/800/200";
+            $newProject->save();
+        }
     }
 }
--- resources/views/admin/create.blade.php
+++ resources/views/admin/create.blade.php
@@ -1,11 +1,23 @@
 @extends('layouts.admin')
 
 @section('content')
-    <form action="{{route('admin.Project.store')}}" method="POST">
+    <form action="{{ route('admin.Project.store') }}" method="POST">
         @csrf
-        <label for="exampleTitle">Inserisci un cazzo di titolo</label>
-        <input type="text" name="title" id="exampleTitle">
-        <button type="submit">  AGGIUNGI </button>
-    </form>
 
+        <div class="w-50 m-5">
+            <label for="formGroupExampleInput" class="form-label">Title</label>
+            <input type="text" class="form-control" id="formGroupExampleInput" placeholder="Project Title" name="title">
+        </div>
+        <div class="w-50 m-5">
+            <label for="formGroupExampleInput2" class="form-label">Description</label>
+            <input type="text" class="form-control" id="formGroupExampleInput2" placeholder="Description"
+                name="description">
+        </div>
+        <div class="w-50 m-5">
+            <label for="formGroupExampleInput2" class="form-label">Image</label>
+            <input type="text" class="form-control" id="formGroupExampleInput2" placeholder="Img URL" name="img">
+        </div>
+
+        <button type="submit"> AGGIUNGI </button>
+    </form>
 @endsection
